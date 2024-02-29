701e00fb100d839aed26fcc90deb5ed79083e0ee
Aryan Mahindra <62005619+Aryan181@users.noreply.github.com>
2024-02-29 00:48:19 -0800

    openai summary working

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from flask import Flask, request, render_template
 import requests
 from bs4 import BeautifulSoup
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Use the OPENAI_API_KEY from the .env file
+openai_api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=openai_api_key)
+
+
 
 app = Flask(__name__)
+
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -15,9 +31,10 @@ def home():
             # Example: Extract all paragraph texts
             paragraphs = soup.find_all('body')
             extracted_text = '\n'.join([p.get_text() for p in paragraphs])
-             
-            print("this is the extracted text")
-            print(extracted_text)
+            summarize_text = getOpenAIresponse(extracted_text)
+            print("here's the summary")
+            print(summarize_text)
+            
             # Display extracted text in preformatted text
             return f'<pre>{extracted_text}</pre>'
         except requests.exceptions.RequestException as e:
@@ -25,5 +42,24 @@ def home():
     else:
         return render_template('index.html')
 
+
+
+
+def getOpenAIresponse(pageText):
+    completion = client.chat.completions.create(
+    model="gpt-4-32k-0613",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant that summarizes text. Summarize this -"},
+        {"role": "user", "content": pageText}
+    ]
+    )
+
+    return (completion.choices[0].message)
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
